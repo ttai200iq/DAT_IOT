6e68cb2fe9cdeb523cfab37453b993445b4aef79
Tai Ngo <ndtt.2712@gmail.com>
2024-04-22 17:08:35 +0700

    TAI

--- src/component/Export/Export.js
+++ src/component/Export/Export.js
@@ -1,26 +1,27 @@
 import React, { useEffect } from "react";
-import "./Export.scss"
+import "./Export.scss";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { signal } from "@preact/signals-react";
 import axios from "axios";
 import { host } from "../constant";
-import ListEx from "./ListEx";
+import ListEx, { configreport } from "./ListEx";
 import ConfigEx from "./ConfigEx";
 import { isBrowser } from "react-device-detect";
 import { MdOutlineWifiTetheringErrorRounded } from "react-icons/md";
 import { IoMdAdd } from "react-icons/io";
-export const exp = signal([])
-export const devicetime = signal('')
-export const reporttime = signal([])
-
+export const exp = signal([]);
+export const devicetime = signal("");
+export const reporttime = signal([]);
 
 export default function Export(props) {
-    const banner = "linear-gradient(140deg, #0061f2, #6900c7)"
-    const icon = <ion-icon name="notifications-outline"></ion-icon>
-    const inf = { code: 'Notif', tit: 'Cài đặt báo cáo' }
-    const [direct, SetDirect] = useState([{ id: 'home', text: 'Trang chủ' }, { id: 'list', text: inf.tit }])
-
+    const banner = "linear-gradient(140deg, #0061f2, #6900c7)";
+    const icon = <ion-icon name="notifications-outline"></ion-icon>;
+    const inf = { code: "Notif", tit: "Cài đặt báo cáo" };
+    const [direct, SetDirect] = useState([
+        { id: "home", text: "Trang chủ" },
+        { id: "list", text: inf.tit },
+    ]);
 
     const [nav, setNav] = useState("list");
 
@@ -28,7 +29,6 @@ export default function Export(props) {
         cur: "blue",
         pre: "black",
     };
-
 
     const handleNav = (e) => {
         var id = e.currentTarget.id;
@@ -38,50 +38,53 @@ export default function Export(props) {
     };
 
     useEffect(() => {
-        console.log(props.username)
-        exp.value = []
-        axios.post(host.DEVICE + "/getErrbyUser", { user: props.username, type: "Project" }, { secure: true, reconnect: true })
+        console.log(props.username);
+        exp.value = [];
+        axios
+            .post(
+                host.DEVICE + "/getErrbyUser",
+                { user: props.username, type: "Project" },
+                { secure: true, reconnect: true }
+            )
             .then((res) => {
-                console.log(res.data)
+                console.log(res.data);
 
-                var listp = res.data
+                var listp = res.data;
 
-                axios.post(host.DEVICE + "/getErrbyUser", { user: props.username, type: "None" }, { secure: true, reconnect: true })
+                axios
+                    .post(
+                        host.DEVICE + "/getErrbyUser",
+                        { user: props.username, type: "None" },
+                        { secure: true, reconnect: true }
+                    )
                     .then((res) => {
-                        console.log(res.data)
+                        console.log(res.data);
 
-                        exp.value = [...listp, ...res.data]
-                        exp.value = exp.value.map((data, index) => ({ ...data, id: index + 1 }))
+                        exp.value = [...listp, ...res.data];
+                        exp.value = exp.value.map((data, index) => ({
+                            ...data,
+                            id: index + 1,
+                        }));
 
-                        // if(res.data[0] !== undefined){        
+                        // if(res.data[0] !== undefined){
                         //     i_.value = res.data[0].id
                         //     register.value = res.data[0].setting
                         // }
+                    });
 
-                    })
-
-                // if(res.data[0] !== undefined){        
+                // if(res.data[0] !== undefined){
                 //     i_.value = res.data[0].id
                 //     register.value = res.data[0].setting
                 // }
-
-            })
-
-
-
-
-
-
-    }, [])
-
-
+            });
+    }, []);
 
     //     axios.post(host.DEVICE + "/getInfErr", { user: props.username }, { secure: true, reconnect: true })
     //         .then((res) => {
     //             console.log(res.data)
     //             reader.value = res.data.map((data, index) => ({ ...data, id: index + 1 })) || []
     //             //list.value = res.data.map((data, index) => ({ ...data, id: index + 1 })) || []
-    //             // if(res.data[0] !== undefined){        
+    //             // if(res.data[0] !== undefined){
     //             //     i_.value = res.data[0].id
     //             //     register.value = res.data[0].setting
     //             // }
@@ -92,30 +95,48 @@ export default function Export(props) {
 
     return (
         <>
-            {isBrowser ?
-                <div className="DAT_Export" >
-                    <div className="DAT_Export_Banner" style={{ backgroundImage: banner, backgroundPosition: "bottom", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+            {isBrowser ? (
+                <div className="DAT_Export">
+                    <div
+                        className="DAT_Export_Banner"
+                        style={{
+                            backgroundImage: banner,
+                            backgroundPosition: "bottom",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                        }}
+                    >
                         {/* <div className="DAT_Export_Banner_Shadow" ></div> */}
                     </div>
 
                     <div className="DAT_Export_Content">
-                        <div className="DAT_Export_Content_Direct" >
+                        <div className="DAT_Export_Content_Direct">
                             {direct.map((data, index) => {
-                                return (
-                                    (index === 0)
-                                        ? <Link key={index} to="/" style={{ textDecoration: 'none', color: "white" }}>
-                                            <span style={{ cursor: "pointer" }}> {data.text}</span>
-                                        </Link>
-                                        : <span key={index} id={data.id + "_DIR"} style={{ cursor: "pointer" }}> {' > ' + data.text}</span>
-
-                                )
+                                return index === 0 ? (
+                                    <Link
+                                        key={index}
+                                        to="/"
+                                        style={{ textDecoration: "none", color: "white" }}
+                                    >
+                                        <span style={{ cursor: "pointer" }}> {data.text}</span>
+                                    </Link>
+                                ) : (
+                                    <span
+                                        key={index}
+                                        id={data.id + "_DIR"}
+                                        style={{ cursor: "pointer" }}
+                                    >
+                                        {" "}
+                                        {" > " + data.text}
+                                    </span>
+                                );
                             })}
                         </div>
                         <div className="DAT_Export_Content_Tit">
-                            <div className="DAT_Export_Content_Tit-icon">
-                                {icon}
+                            <div className="DAT_Export_Content_Tit-icon">{icon}</div>
+                            <div className="DAT_Export_Content_Tit-content">
+                                Cài đặt báo cáo
                             </div>
-                            <div className="DAT_Export_Content_Tit-content" >Cài đặt báo cáo</div>
                         </div>
                         {/* Nav */}
                         <div className="DAT_Export_Content_Main">
@@ -166,12 +187,12 @@ export default function Export(props) {
                         </div>
                     </div>
                 </div>
-                :
+            ) : (
                 <>
                     <div className="DAT_ListDetail">
                         <div className="DAT_ListDetail_HeadTit">
                             <MdOutlineWifiTetheringErrorRounded size={25} color="grey" />
-                            <span >{inf.tit}</span>
+                            <span>{inf.tit}</span>
                         </div>
                         <div className="DAT_ListDetail_Content">
                             <div className="DAT_ListDetail_Content_Filterbar">
@@ -189,7 +210,12 @@ export default function Export(props) {
                             </div>
 
                             <div className="DAT_Export_Content_Main_List">
-                                {(() => {
+                                {configreport.value === false ?
+                                    <ListEx username={props.username} />
+                                    :
+                                    <ConfigEx username={props.username} />
+                                }
+                                {/* {(() => {
                                     switch (nav) {
                                         case "list":
                                             return (
@@ -198,7 +224,9 @@ export default function Export(props) {
                                                         <div
                                                             className="DAT_ListDetail_Content_Menu_Item1"
                                                             id="list"
-                                                            style={{ color: nav === "list" ? color.cur : color.pre }}
+                                                            style={{
+                                                                color: nav === "list" ? color.cur : color.pre,
+                                                            }}
                                                             onClick={(e) => {
                                                                 handleNav(e);
                                                             }}
@@ -209,7 +237,9 @@ export default function Export(props) {
                                                         <div
                                                             className="DAT_ListDetail_Content_Menu_Item2"
                                                             id="config"
-                                                            style={{ color: nav === "config" ? color.cur : color.pre }}
+                                                            style={{
+                                                                color: nav === "config" ? color.cur : color.pre,
+                                                            }}
                                                             onClick={(e) => {
                                                                 handleNav(e);
                                                             }}
@@ -227,7 +257,9 @@ export default function Export(props) {
                                                         <div
                                                             className="DAT_ListDetail_Content_Menu_Item1"
                                                             id="list"
-                                                            style={{ color: nav === "list" ? color.cur : color.pre }}
+                                                            style={{
+                                                                color: nav === "list" ? color.cur : color.pre,
+                                                            }}
                                                             onClick={(e) => {
                                                                 handleNav(e);
                                                             }}
@@ -238,7 +270,9 @@ export default function Export(props) {
                                                         <div
                                                             className="DAT_ListDetail_Content_Menu_Item2"
                                                             id="config"
-                                                            style={{ color: nav === "config" ? color.cur : color.pre }}
+                                                            style={{
+                                                                color: nav === "config" ? color.cur : color.pre,
+                                                            }}
                                                             onClick={(e) => {
                                                                 handleNav(e);
                                                             }}
@@ -252,12 +286,12 @@ export default function Export(props) {
                                         default:
                                             <></>;
                                     }
-                                })()}
+                                })()} */}
                             </div>
                         </div>
-                    </div >
+                    </div>
                 </>
-            }
+            )}
         </>
-    )
+    );
 }
