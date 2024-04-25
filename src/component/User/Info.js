6ef08b6d67b9e0534f5545b2e53edef8b68a9870
Tai Ngo <ndtt.2712@gmail.com>
2024-04-25 14:30:49 +0700

    Tai

--- src/component/User/Info.js
+++ src/component/User/Info.js
@@ -7,10 +7,11 @@ import { useIntl } from "react-intl";
 import { AlertContext } from "../Context/AlertContext";
 import { IoClose } from "react-icons/io5";
 import { editUser } from "./User";
+import { data } from "./Listuser";
 
 export default function Info() {
-    const type = useSelector((state) => state.admin.type)
-    const user = useSelector((state) => state.admin.user)
+    const type = useSelector((state) => state.admin.type);
+    const user = useSelector((state) => state.admin.user);
     const userName = useRef("");
     const mail = useRef("");
     const pass = useRef("");
@@ -18,49 +19,97 @@ export default function Info() {
     const fullname = useRef("");
     const { alertDispatch } = useContext(AlertContext);
     const dataLang = useIntl();
-    const [gettype, setGettype] = useState('')
+    const [gettype, setGettype] = useState("");
 
 
     useEffect(() => {
-        if (type === 'master') {
-            setGettype('admin')
+        if (type === "master") {
+            setGettype("admin");
         } else {
-            setGettype('user')
+            setGettype("user");
         }
-    }, [])
-
+    }, []);
 
     const handleSave = (e) => {
         e.preventDefault();
         if (pass.current.value === authpass.current.value) {
-
-
-            axios.post(host.DEVICE + "/createAcount", { user: userName.current.value, username: fullname.current.value, mail: mail.current.value, pass: authpass.current.value, type: gettype, admin: user, manager: userName.current.value }, { withCredentials: true }).then(
-                function (res) {
-                    console.log(res.data)
+            axios
+                .post(
+                    host.DEVICE + "/createAcount",
+                    {
+                        user: userName.current.value,
+                        username: fullname.current.value,
+                        mail: mail.current.value,
+                        pass: authpass.current.value,
+                        type: gettype,
+                        admin: user,
+                        manager: userName.current.value,
+                    },
+                    { withCredentials: true }
+                )
+                .then(function (res) {
+                    console.log(res.data);
                     if (res.data.status) {
-                        alertDispatch({ type: 'LOAD_CONTENT', payload: { content: dataLang.formatMessage({ id: "alert_12" }), show: 'block' } })
+                        alertDispatch({
+                            type: "LOAD_CONTENT",
+                            payload: {
+                                content: dataLang.formatMessage({ id: "alert_12" }),
+                                show: "block",
+                            },
+                        });
+                        editUser.value = false;
+
+                        axios.post(host.DEVICE + "/getAcount", { admin: user }, { withCredentials: true }).then(
+                            function (res) {
+                                var newData = res.data.map((data, index) => {
+                                    data["id"] = index + 1;
+                                    return data;
+                                });
+                                data.value = newData;
+                            })
+
                     } else {
                         switch (res.data.number) {
                             case 1:
-                                alertDispatch({ type: 'LOAD_CONTENT', payload: { content: dataLang.formatMessage({ id: "alert_10" }), show: 'block' } })
-                                break
+                                alertDispatch({
+                                    type: "LOAD_CONTENT",
+                                    payload: {
+                                        content: dataLang.formatMessage({ id: "alert_10" }),
+                                        show: "block",
+                                    },
+                                });
+                                break;
                             case 2:
-                                alertDispatch({ type: 'LOAD_CONTENT', payload: { content: dataLang.formatMessage({ id: "alert_11" }), show: 'block' } })
-                                break
+                                alertDispatch({
+                                    type: "LOAD_CONTENT",
+                                    payload: {
+                                        content: dataLang.formatMessage({ id: "alert_11" }),
+                                        show: "block",
+                                    },
+                                });
+                                break;
                             default:
-                                alertDispatch({ type: 'LOAD_CONTENT', payload: { content: dataLang.formatMessage({ id: "alert_3" }), show: 'block' } })
-                                break
-
+                                alertDispatch({
+                                    type: "LOAD_CONTENT",
+                                    payload: {
+                                        content: dataLang.formatMessage({ id: "alert_3" }),
+                                        show: "block",
+                                    },
+                                });
+                                break;
                         }
                     }
-                })
-
+                });
         } else {
-            alertDispatch({ type: 'LOAD_CONTENT', payload: { content: dataLang.formatMessage({ id: "alert_22" }), show: 'block' } })
+            alertDispatch({
+                type: "LOAD_CONTENT",
+                payload: {
+                    content: dataLang.formatMessage({ id: "alert_22" }),
+                    show: "block",
+                },
+            });
         }
         //console.log(userName.current.value, mail.current.value, pass.current.value, authpass.current.value, fullname.current.value, id.current.value)
-
     };
 
     return (
@@ -72,23 +121,26 @@ export default function Info() {
                     </div>
                     <div className="DAT_Info_Form_Head_Right">
                         <div className="DAT_Info_Form_Head_Right_Close">
-                            <span style={{
-                                width: "30px",
-                                height: "30px",
-                                borderRadius: "50%",
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                backgroundColor: "red"
-                            }}
-                                onClick={() => editUser.value = false}>
+                            <span
+                                style={{
+                                    width: "30px",
+                                    height: "30px",
+                                    borderRadius: "50%",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    backgroundColor: "red",
+                                }}
+                                onClick={() => (editUser.value = false)}
+                            >
                                 <IoClose size={20} color="white" />
                             </span>
                         </div>
                     </div>
                 </div>
-                <div className="DAT_Info_Form_Row"
-                    style={{ borderRadius: '5px 5px 0 0' }}
+                <div
+                    className="DAT_Info_Form_Row"
+                    style={{ borderRadius: "5px 5px 0 0" }}
                 >
                     <div className="DAT_Info_Form_Row_Item">
                         <div className="DAT_Info_Form_Row_Item_Label">Tên</div>
@@ -104,32 +156,49 @@ export default function Info() {
                 <div className="DAT_Info_Form_Row">
                     <div className="DAT_Info_Form_Row_Item">
                         <div className="DAT_Info_Form_Row_Item_Label">Tài khoản</div>
-                        <input type="text" placeholder="Tài khoản" ref={userName} required />
+                        <input
+                            type="text"
+                            placeholder="Tài khoản"
+                            ref={userName}
+                            required
+                        />
                     </div>
                 </div>
 
                 <div className="DAT_Info_Form_Row">
                     <div className="DAT_Info_Form_Row_Item">
                         <div className="DAT_Info_Form_Row_Item_Label">Mật khẩu</div>
-                        <input type="password" placeholder="Nhập mật khẩu" ref={pass} required />
+                        <input
+                            type="password"
+                            placeholder="Nhập mật khẩu"
+                            ref={pass}
+                            required
+                        />
                     </div>
                 </div>
                 <div className="DAT_Info_Form_Row">
                     <div className="DAT_Info_Form_Row_Item">
-                        <div className="DAT_Info_Form_Row_Item_Label">Xác nhận mật khẩu</div>
-                        <input type="password" placeholder="Nhập lại mật khẩu" ref={authpass} required />
+                        <div className="DAT_Info_Form_Row_Item_Label">
+                            Xác nhận mật khẩu
+                        </div>
+                        <input
+                            type="password"
+                            placeholder="Nhập lại mật khẩu"
+                            ref={authpass}
+                            required
+                        />
                     </div>
                 </div>
 
-                <div className="DAT_Info_Form_Row"
-                    style={{ borderRadius: '0 0 5px 5px' }}
+                <div
+                    className="DAT_Info_Form_Row"
+                    style={{ borderRadius: "0 0 5px 5px" }}
                 >
-                    <button className="DAT_Info_Form_Row_Button" >
+                    <button className="DAT_Info_Form_Row_Button">
                         <ion-icon name="save-outline"></ion-icon>Thêm
                     </button>
                 </div>
             </form>
         </div>
-
     );
 }
